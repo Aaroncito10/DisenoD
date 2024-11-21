f53f5bc7346642db12789215437c51a147761cbe
Aaroncito10 <168374858+Aaroncito10@users.noreply.github.com>
2024-11-20 19:28:49 -0600

    Add files via upload

---
+++ EjercicioIntegrador/src/ejerciciointegrador/vendedor.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejerciciointegrador;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author aaron
+ */
+public class vendedor {
+    private String [] Produc;
+    private int [] cantidadVentas;
+    
+    
+    public void ventas() {
+        int cantidadProduc;
+        String productosVenta[];
+
+        System.out.println("Bienvenido, ingrese por favor los productos que desea ingresar: ");
+
+        cantidadProduc = Integer.parseInt(JOptionPane.showInputDialog("Ingrese los productos que desea ingresar a su arreglo: "));
+
+        
+        
+    }
+}
